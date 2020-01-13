be5fa4266290361719f021e2a98986ccee0bc21a
Ben Calvert <benhcalvert@gmail.com>
2020-01-13 09:09:26 -0700

    loop to retrieve from localstorage

--- HW5/assets/script.js
+++ HW5/assets/script.js
@@ -18,13 +18,24 @@ $(document).ready(function () {
         localStorage.setItem(click, eventInput);
     });
 
+    // load from local storage on page load
+
+    // on refresh, loop through time block and grab values from local storage 'getItem' what happens if you don't have a value in a certain time block (this may or may not happen).
+    let timeBlock = ["#9am", "#10am", "#11am", "#12pm", "#1pm", "#2pm", "#3pm", "#4pm", "#5pm"];
+    
+    for (let i = 0; i < timeBlock.length; i++) {
+        let savedEvent = $('.saved-event')
+        console.log('looping through time block to retrieve LS');        
+        $(timeBlock[i]).val(localStorage.getItem(timeBlock[i]));
+    };
+    
+
     // Change style base on time
     function timeStyle() {
         for (let i = 6; i < 18; i++) {
 
             let hour = '#' + i;
             let scheduleHour = parseInt($(hour).attr("id"));
-            // console.log('schedule hour: ', scheduleHour);
             $(hour).removeClass();
             if (
                 scheduleHour > currentHour) {
@@ -44,33 +55,6 @@ $(document).ready(function () {
     timeStyle();
 
     // on refresh, loop through time block and grab values from local storage 'getItem' what happens if you don't have a value in a certain time block (this may or may not happen).
-    var timeBlock = ["#9am", "#10am", "#11am", "#12pm", "#1pm", "#2pm", "#3pm", "#4pm", "#5pm"];
+    // var timeBlock = ["#9am", "#10am", "#11am", "#12pm", "#1pm", "#2pm", "#3pm", "#4pm", "#5pm"];
 
-
-
-
-    // create time rows in JS
-    //timeblocks -----
-
-
-
-
-
-    // Loop to create row for each hour
-    // for(var i = 0; i < timeBlock.length; i++){
-    //     var makeEventDiv = $("<div class='col-2'></div>");
-    //     var makeEventButton = $("<button>")
-    //     var timeDiv = $("<div class='col-2' value='" + timeBlock[i] + "'><p></p></div>");
-    //     var eventDiv = $("<div class='col-8'><input type='text' id='eventInput' data-value='" + timeBlock[i] + "'></div>");
-    //     makeEventButton.addClass("event-button");
-    //     makeEventButton.text("Save");
-    //     timeDiv.text(timeBlock[i]);      
-    //     $(".row").append(timeDiv);
-    //     $(".row").append(eventDiv);
-    //     $(".row").append(makeEventButton);
-    //     console.log("making buttons");
-    // }
-
-
-
-})
+});
